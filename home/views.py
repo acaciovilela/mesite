c0f5f7eb616200ac9748b20f20a0f051fa34520f
Acácio Vilela <acaciovilela@gmail.com>
2020-05-08 14:16:46 -0300

    Update

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from dtlblog.models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html', {})
-
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    return render(request, 'home/index.html', {'posts': posts})
